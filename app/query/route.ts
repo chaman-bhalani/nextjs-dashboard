b50857624a0b026451a84c636977931fb7123b31
chaman_bhalani <108402674+chaman-bhalani@users.noreply.github.com>
2025-06-20 17:02:05 +0530

    displaying dashboard content and adding suspense boundaries

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,14 +1,14 @@
+import { NextResponse } from "next/server";
 import postgres from "postgres";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
-
-async function listInvoices() {
+export async function GET() {
   const data = await sql`
     SELECT invoices.amount, customers.name
     FROM invoices
     JOIN customers ON invoices.customer_id = customers.id
-    WHERE invoices.amount = 666;
+    WHERE invoices.amount = ${666};
   `;
 
-  return data;
+  return NextResponse.json(data);
 }
