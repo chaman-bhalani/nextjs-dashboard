40d9b365dc6201e6842d285d1c466a3df7c35084
chaman_bhalani <108402674+chaman-bhalani@users.noreply.github.com>
2025-06-24 14:49:05 +0530

    added metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Acme Customers Page",
+  description: "View Customers ",
+};
 export default function Page() {
   return <p>Customers Page</p>;
 }
